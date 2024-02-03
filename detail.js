2279e860bef100c721b2cf3941abdf7af3fbe771
SeoMiYoung <seo20seo00@naver.com>
2024-02-03 22:05:06 +0900

    탭기능을 for문으로 코드 단축시키기

--- detail.js
+++ detail.js
@@ -4,41 +4,20 @@
     // - 모든 div에 붙은 show 클래스명 제거 
     // - div0에 show 클래스명 추가
 
-let productsBtn = document.getElementsByClassName('tab-button')[0];
-let informationBtn = document.getElementsByClassName('tab-button')[1];
-let shippingBtn = document.getElementsByClassName('tab-button')[2];
+let tabButtons = document.getElementsByClassName('tab-button');
+let tabContents = document.getElementsByClassName('tab-content');
 
-let productsContent = document.getElementsByClassName('tab-content')[0];
-let informationContent = document.getElementsByClassName('tab-content')[1];
-let shippingContent = document.getElementsByClassName('tab-content')[2];
-
-productsBtn.addEventListener('click', function() {
-    // orange 조절
-    productsBtn.classList.add('orange');
-    informationBtn.classList.remove('orange');
-    shippingBtn.classList.remove('orange');
-
-    // div 조절
-    productsContent.classList.add('show');
-    informationContent.classList.remove('show');
-    shippingContent.classList.remove('show');
-});
-informationBtn.addEventListener('click', function() {
-    productsBtn.classList.remove('orange');
-    informationBtn.classList.add('orange');
-    shippingBtn.classList.remove('orange');
-
-    productsContent.classList.remove('show');
-    informationContent.classList.add('show');
-    shippingContent.classList.remove('show');
-});
-shippingBtn.addEventListener('click', function() {
-    productsBtn.classList.remove('orange');
-    informationBtn.classList.remove('orange');
-    shippingBtn.classList.add('orange');
-
-    productsContent.classList.remove('show');
-    informationContent.classList.remove('show');
-    shippingContent.classList.add('show');
-});
-
+for(let n=0; n<tabButtons.length; n++) {
+    tabButtons[n].addEventListener('click', function() {
+        for(let i=0; i<tabButtons.length; i++) {
+            if(n==i) {
+                tabButtons[i].classList.add('orange');
+                tabContents[i].classList.add('show');
+            }
+            else {
+                tabButtons[i].classList.remove('orange');
+                tabContents[i].classList.remove('show');
+            }
+        }
+    });
+}
