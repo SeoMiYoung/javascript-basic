6d70156cf6eb124d40b8bf8532aaef74a7ac5610
SeoMiYoung <seo20seo00@naver.com>
2024-02-09 15:57:05 +0900

    [myself] 스크롤 위치에 따른 transform 속성의 scale조절

--- scrollAnimation/scroll.js
+++ scrollAnimation/scroll.js
@@ -7,8 +7,16 @@ window.addEventListener('scroll', function() {
     // 650~1150까지 스크롤바를 내리면, 첫번째 카드의 opacity 속성을 서서히
     // 1에서 0으로 변경해주셈
 
-    // x값이 650->1150 변할 때, y값은 0->1로 변한게 하고싶다면?
+    // x값이 650->1150 변할 때, y값은 0->1로 변하게 하고싶다면?
     let y = (-1/500)*height + (115/50);
+
+    // x값이 650->1150 변할 때, transform의 scale을 0.9로 만들고 싶다면?
+    let scale = (-1/5000)*height + (113/100);
+
+    // opacity 조절
     document.getElementsByClassName('card-box')[0].style.opacity = y;
+
+    // scale 조절
+    document.getElementsByClassName('card-box')[0].style.transform = 'scale('+scale+')';
     
 });
